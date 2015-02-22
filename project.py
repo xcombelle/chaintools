5aa059a33daeab7f2eb703950dd2ab8e35b04c63
Xavier Combelle <xavier.combelle@gmail.com>
2015-02-22 16:20:18 +0100

    code beaucouop simplifié

--- project.py
+++ project.py
@@ -1,43 +1,34 @@
 #!myfunction(param1="a",param2="b")|prog {param1} "param2"
+import fileinput
+def cat(*names):
+    def cat(input):
+        yield from fileinput.input(*names)
+    return cat
 
-def cat(*names,input=None,stderr=None):
-    
-    import fileinput
-    yield from fileinput.input(*names)
-    
-def split(separator="",input=None,stderr=None):
+def output():
+    def _output(input):
+        for line in input:
+            print(line,end="")
+    return _output
+def split(separator=""):
     #print("splitting",file=stderr)
-    yield from (line.split(separator) for line in input)
+    def _split(input):
+        yield from (line.split(separator) for line in input)
+    return _split
+def sort(key=None):
+    def _sort(input):
+        yield from sorted(input,key=key)
+    return _sort
+def join(separator=""):
+    def _join(input):
+        yield from (separator.join(line) for line in input)
+    return _join
 
-def sort(key=None,input=None,stderr=None):
-    yield from sorted((line for line in input),key=key)
-
-def join(separator="",input=None,stderr=None):
-    yield from (separator.join(line) for line in input)
 import sys
-#!cat(sys.argv[1:])|split(",")|sort(key=lambda k:int(k[1]))|join("\t") 
-def _private_pipe():
-    import sys
-    import functools
-    _private_stderr = sys.stderr
-    _private_input = []
-    #print ("ici")
-    def _private_make_gen(_private_input,_private_generator,*args,**kwargs):
-        #print(generator.__name__)
-        #print("la")
-        #print("input:",input)
-        #print("args:",args)
-        #print("kwargs:",kwargs)
-        for line in functools.partial(_private_generator,
-                                      input=_private_input,
-                                      stderr=_private_stderr)(*args,**kwargs):
-            #print ("debug:",repr(line))
-            yield line
-    
-    _private_input = _private_make_gen(_private_input,cat,sys.argv[1:])
-    _private_input = _private_make_gen(_private_input,split,",")
-    _private_input = _private_make_gen(_private_input,sort,key=lambda k:int(k[1]))
-    _private_input = _private_make_gen(_private_input,join,"\t")
-    for line in _private_input:
-        print (line,end="")
-_private_pipe()
+def chain_generators(*generators):
+    input = None
+    for generator in generators:
+        input = generator(input)
+    return generator
+
+chain_generators(cat(sys.argv[1:]),split(","),sort(key=lambda k:int(k[1])),join("\t"),output())
