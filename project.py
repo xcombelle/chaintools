889e7172449706242d97d9b9ed2c2464c0f936c6
Xavier Combelle <xavier.combelle@gmail.com>
2015-04-25 11:47:59 +0200

    first happy version (correct commit)

--- project.py
+++ project.py
@@ -1,34 +1,43 @@
 #!myfunction(param1="a",param2="b")|prog {param1} "param2"
 import fileinput
+import sys
+
 def cat(*names):
-    def cat(input):
+    def _cat(input):
         yield from fileinput.input(*names)
-    return cat
+    return _cat
 
 def output():
     def _output(input):
         for line in input:
             print(line,end="")
     return _output
-def split(separator=""):
+
+def split(separator=None):
     #print("splitting",file=stderr)
     def _split(input):
         yield from (line.split(separator) for line in input)
     return _split
-def sort(key=None):
+
+def sort(key=None,reverse=False):
     def _sort(input):
-        yield from sorted(input,key=key)
+        yield from sorted(input,key=key,reverse=reverse)
     return _sort
+
 def join(separator=""):
     def _join(input):
         yield from (separator.join(line) for line in input)
     return _join
 
-import sys
+
 def chain_generators(*generators):
     input = None
     for generator in generators:
         input = generator(input)
     return generator
 
-chain_generators(cat(sys.argv[1:]),split(","),sort(key=lambda k:int(k[1])),join("\t"),output())
+chain_generators(cat(sys.argv[1:]),
+                 split(","),
+                 sort(key=lambda k:int(k[1]),reverse=True),
+                 join("\t"),
+                 output())
