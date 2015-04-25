3c16e6f420757d78453d9de096278d070162a155
Xavier Combelle <xavier.combelle@gmail.com>
2015-04-25 11:50:22 +0200

    reddit functionnality

--- project.py
+++ project.py
@@ -2,19 +2,25 @@
 import fileinput
 import sys
 
+
 def cat(*names):
     def _cat(input):
-        yield from fileinput.input(*names)
+        if names:
+            input = fileinput.input(*names)
+        else:
+            input = sys.stdin
+        for elt in input:
+            yield elt.rstrip("\n")
+               
     return _cat
 
-def output():
+def output(end=None):
     def _output(input):
         for line in input:
-            print(line,end="")
+            print(line,end=end)
     return _output
 
 def split(separator=None):
-    #print("splitting",file=stderr)
     def _split(input):
         yield from (line.split(separator) for line in input)
     return _split
@@ -36,8 +42,40 @@ def chain_generators(*generators):
         input = generator(input)
     return generator
 
-chain_generators(cat(sys.argv[1:]),
-                 split(","),
-                 sort(key=lambda k:int(k[1]),reverse=True),
-                 join("\t"),
-                 output())
+
+def filter(func):
+    def _filter(input):
+        yield from (elt for elt in input if func(elt)) 
+    return _filter
+    
+def map(func):
+    def _map(input):
+        yield from (func(elt) for elt in input)
+    return _map
+
+def take(n):
+    def _take(input):
+        for i,elt in enumerate(input):
+            yield elt 
+            if i == n:
+                break
+    return _take
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "1":
+        chain_generators(cat(sys.argv[2:]),
+                         split(","),
+                         sort(key=lambda k:int(k[1]),reverse=True),
+                         join("\t"),
+                         output())
+
+    elif sys.argv[1] == "2":
+        chain_generators(cat(),
+                         filter(lambda s: s and s[0] != '#'),
+                         map(float),
+                         sort(),
+                         take(10),
+                         output())
+                     
+#endnew
